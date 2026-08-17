810be2af403a07a7a6039d875c50f88b0d3cf19d
ramluovs <150495546+ramluovs@users.noreply.github.com>
2026-08-17 14:16:37 -0400

    Update names.js

--- commands/roblox/names.js
+++ commands/roblox/names.js
@@ -56,7 +56,7 @@ module.exports = {
     const data = await res.json();
     const history = data.data || [];
     const profileUrl = `https://www.roblox.com/users/${target.id}/profile`;
-    const titleText = `nombres pasados de ${target.name} (${history.length})`;
+    const titleText = `${target.name} (${history.length}) — nombres pasados `;
 
     if (history.length === 0) {
       return message.reply({
